2eb39e7a794f01404d43beb865aaf2fb9965f1f5
danikaze <danikaze@gmail.com>
2020-08-12 09:14:15 +0900

    Don't type translation data to allow it to be defined in runtime

--- scripts/test-locale/data.ts
+++ scripts/test-locale/data.ts
@@ -3,30 +3,28 @@
 import { TranslationData } from '@src/utils/i18n';
 import { formatLy, formatCredits } from '@src/utils/format';
 
-export type TestCase<
-  K extends keyof TranslationData = keyof TranslationData
-> = {
-  key: K;
-  data: TranslationData[K];
+export type TestCase = {
+  key: string;
+  data: TranslationData;
 };
 
 export const testCases: TestCase[] = [
   {
     key: 'bodiesApproached',
     data: { sessionTotalBodiesApproached: 1 },
-  } as TestCase<'bodiesApproached'>,
+  },
   {
     key: 'bodiesApproached',
     data: { sessionTotalBodiesApproached: 5 },
-  } as TestCase<'bodiesApproached'>,
+  },
   {
     key: 'bodiesLeft',
     data: { sessionTotalBodiesLeft: 1 },
-  } as TestCase<'bodiesLeft'>,
+  },
   {
     key: 'bodiesLeft',
     data: { sessionTotalBodiesLeft: 2 },
-  } as TestCase<'bodiesLeft'>,
+  },
   {
     key: 'bounty',
     data: {
@@ -37,7 +35,7 @@ export const testCases: TestCase[] = [
       sessionTotalBountyCr: formatCredits(12345),
       sessionTotalPiratesKilled: 1,
     },
-  } as TestCase<'bounty'>,
+  },
   {
     key: 'bounty',
     data: {
@@ -48,232 +46,232 @@ export const testCases: TestCase[] = [
       sessionTotalBountyCr: formatCredits(12345678),
       sessionTotalPiratesKilled: 8,
     },
-  } as TestCase<'bounty'>,
+  },
   {
     key: 'dockingsDenied',
     data: {
       sessionTotalDockingsDenied: 1,
       lastDockingDeniedReason: 'NoSpace',
     },
-  } as TestCase<'dockingsDenied'>,
+  },
   {
     key: 'dockingsDenied',
     data: {
       sessionTotalDockingsDenied: 3,
       lastDockingDeniedReason: 'Offences',
     },
-  } as TestCase<'dockingsDenied'>,
+  },
   {
     key: 'dockingsGranted',
     data: {
       sessionTotalDockingsGranted: 1,
     },
-  } as TestCase<'dockingsGranted'>,
+  },
   {
     key: 'dockingsGranted',
     data: {
       sessionTotalDockingsGranted: 2,
     },
-  } as TestCase<'dockingsGranted'>,
+  },
   {
     key: 'dockingsRequested',
     data: {
       sessionTotalDockingsRequested: 1,
     },
-  } as TestCase<'dockingsRequested'>,
+  },
   {
     key: 'dockingsRequested',
     data: {
       sessionTotalDockingsRequested: 4,
     },
-  } as TestCase<'dockingsRequested'>,
+  },
   {
     key: 'heatWarning',
     data: {
       sessionTotalHeatWarnings: 1,
     },
-  } as TestCase<'heatWarning'>,
+  },
   {
     key: 'heatWarning',
     data: {
       sessionTotalHeatWarnings: 3,
     },
-  } as TestCase<'heatWarning'>,
+  },
   {
     key: 'interdictionsEscaped',
     data: {
       sessionTotalInterdictionsReceivedEscaped: 1,
     },
-  } as TestCase<'interdictionsEscaped'>,
+  },
   {
     key: 'interdictionsEscaped',
     data: {
       sessionTotalInterdictionsReceivedEscaped: 3,
     },
-  } as TestCase<'interdictionsEscaped'>,
+  },
   {
     key: 'interdictionsLost',
     data: {
       sessionTotalInterdictionsReceivedLost: 1,
     },
-  } as TestCase<'interdictionsLost'>,
+  },
   {
     key: 'interdictionsLost',
     data: {
       sessionTotalInterdictionsReceivedLost: 2,
     },
-  } as TestCase<'interdictionsLost'>,
+  },
   {
     key: 'interdictionsSubmitted',
     data: {
       sessionTotalInterdictionsReceivedSubmitted: 1,
     },
-  } as TestCase<'interdictionsSubmitted'>,
+  },
   {
     key: 'interdictionsSubmitted',
     data: {
       sessionTotalInterdictionsReceivedSubmitted: 10,
     },
-  } as TestCase<'interdictionsSubmitted'>,
+  },
   {
     key: 'jumpDistance',
     data: {
       sessionTotalJumpDistance: 150.123456789,
       sessionTotalJumpDistanceLy: formatLy(150.123456789),
     },
-  } as TestCase<'jumpDistance'>,
+  },
   {
     key: 'launchedDrones',
     data: {
       sessionTotalDronesLaunched: 1,
     },
-  } as TestCase<'launchedDrones'>,
+  },
   {
     key: 'launchedDrones',
     data: {
       sessionTotalDronesLaunched: 19,
     },
-  } as TestCase<'launchedDrones'>,
+  },
   {
     key: 'materialCollected',
     data: {
       sessionTotalMaterialsCollected: 1,
     },
-  } as TestCase<'materialCollected'>,
+  },
   {
     key: 'materialCollected',
     data: {
       sessionTotalMaterialsCollected: 235,
     },
-  } as TestCase<'materialCollected'>,
+  },
   {
     key: 'miningRefined',
     data: {
       sessionTotalMiningRefined: 1,
     },
-  } as TestCase<'miningRefined'>,
+  },
   {
     key: 'miningRefined',
     data: {
       sessionTotalMiningRefined: 395,
     },
-  } as TestCase<'miningRefined'>,
+  },
   {
     key: 'missionsAbandoned',
     data: {
       sessionTotalMissionsAbandoned: 1,
     },
-  } as TestCase<'missionsAbandoned'>,
+  },
   {
     key: 'missionsAbandoned',
     data: {
       sessionTotalMissionsAbandoned: 3,
     },
-  } as TestCase<'missionsAbandoned'>,
+  },
   {
     key: 'missionsAccepted',
     data: {
       sessionTotalMissionsAccepted: 1,
     },
-  } as TestCase<'missionsAccepted'>,
+  },
   {
     key: 'missionsAccepted',
     data: {
       sessionTotalMissionsAccepted: 20,
     },
-  } as TestCase<'missionsAccepted'>,
+  },
   {
     key: 'missionsCompleted',
     data: {
       sessionTotalMissionsCompleted: 1,
     },
-  } as TestCase<'missionsCompleted'>,
+  },
   {
     key: 'missionsCompleted',
     data: {
       sessionTotalMissionsCompleted: 10,
     },
-  } as TestCase<'missionsCompleted'>,
+  },
   {
     key: 'missionsFailed',
     data: {
       sessionTotalMissionsFailed: 1,
     },
-  } as TestCase<'missionsFailed'>,
+  },
   {
     key: 'missionsFailed',
     data: {
       sessionTotalMissionsFailed: 3,
     },
-  } as TestCase<'missionsFailed'>,
+  },
   {
     key: 'prospectedAsteroids',
     data: {
       sessionTotalAsteroidsProspected: 1,
     },
-  } as TestCase<'prospectedAsteroids'>,
+  },
   {
     key: 'prospectedAsteroids',
     data: {
       sessionTotalAsteroidsProspected: 4,
     },
-  } as TestCase<'prospectedAsteroids'>,
+  },
   {
     key: 'scanned',
     data: {
       sessionTotalScanned: 1,
     },
-  } as TestCase<'scanned'>,
+  },
   {
     key: 'scanned',
     data: {
       sessionTotalScanned: 15,
     },
-  } as TestCase<'scanned'>,
+  },
   {
     key: 'detailedScan',
     data: {
       sessionTotalDetailedScans: 1,
     },
-  } as TestCase<'detailedScan'>,
+  },
   {
     key: 'detailedScan',
     data: {
       sessionTotalDetailedScans: 23,
     },
-  } as TestCase<'detailedScan'>,
+  },
   {
     key: 'cargoScan',
     data: {
       sessionTotalCargoScans: 1,
     },
-  } as TestCase<'cargoScan'>,
+  },
   {
     key: 'cargoScan',
     data: {
       sessionTotalCargoScans: 23,
     },
-  } as TestCase<'cargoScan'>,
+  },
   {
     key: 'soldExplorationData',
     data: {
@@ -283,7 +281,7 @@ export const testCases: TestCase[] = [
       lastSoldExplorationDataValueCr: formatCredits(150000),
       sessionTotalSoldExplorationData: 1,
     },
-  } as TestCase<'soldExplorationData'>,
+  },
   {
     key: 'soldExplorationData',
     data: {
@@ -293,5 +291,5 @@ export const testCases: TestCase[] = [
       lastSoldExplorationDataValueCr: formatCredits(654321),
       sessionTotalSoldExplorationData: 5,
     },
-  } as TestCase<'soldExplorationData'>,
+  },
 ];
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,56 +1,8 @@
 import * as i18n from 'i18n';
 import { LOCALES_FOLDER } from '@src/constants';
 
-import { TranslationData as BodiesApproachedData } from '@src/info-generators/bodies-approached';
-import { TranslationData as BodiesLeftData } from '@src/info-generators/bodies-left';
-import { TranslationData as BountyData } from '@src/info-generators/bounty';
-import { TranslationData as FactionKillBondsData } from '@src/info-generators/faction-kill-bond';
-import { TranslationData as DockingsDeniedData } from '@src/info-generators/dockings-denied';
-import { TranslationData as DockingsGrantedData } from '@src/info-generators/dockings-granted';
-import { TranslationData as DockingsRequestedData } from '@src/info-generators/dockings-requested';
-import { TranslationData as HeatWarningData } from '@src/info-generators/heat-warning';
-import { TranslationData as InterdictionsEscapedData } from '@src/info-generators/interdiction-escaped';
-import { TranslationData as InterdictionsLostData } from '@src/info-generators/interdiction-lost';
-import { TranslationData as InterdictionsSubmittedData } from '@src/info-generators/interdiction-submitted';
-import { TranslationData as JumpDistanceData } from '@src/info-generators/jump-distance';
-import { TranslationData as LaunchedDronesData } from '@src/info-generators/launched-drones';
-import { TranslationData as MaterialCollectedData } from '@src/info-generators/material-collected';
-import { TranslationData as MiningRefinedData } from '@src/info-generators/mining-refined';
-import { TranslationData as MissionsAbandonedData } from '@src/info-generators/missions-abandoned';
-import { TranslationData as MissionsAcceptedData } from '@src/info-generators/missions-accepted';
-import { TranslationData as MissionsCompletedData } from '@src/info-generators/missions-completed';
-import { TranslationData as MissionsFailedData } from '@src/info-generators/missions-failed';
-import { TranslationData as ProspectedAsteroidsData } from '@src/info-generators/prospected-asteroids';
-import { TranslationData as ScannedData } from '@src/info-generators/scanned';
-import { TranslationData as DetailedScanData } from '@src/info-generators/scan-detailed';
-import { TranslationData as CargoScanData } from '@src/info-generators/scan-cargo';
-import { TranslationData as SoldExplorationDataData } from '@src/info-generators/exploration-data-sold';
-
 export interface TranslationData {
-  bodiesApproached: BodiesApproachedData;
-  bodiesLeft: BodiesLeftData;
-  bounty: BountyData;
-  factionKillBonds: FactionKillBondsData;
-  dockingsDenied: DockingsDeniedData;
-  dockingsGranted: DockingsGrantedData;
-  dockingsRequested: DockingsRequestedData;
-  heatWarning: HeatWarningData;
-  interdictionsEscaped: InterdictionsEscapedData;
-  interdictionsLost: InterdictionsLostData;
-  interdictionsSubmitted: InterdictionsSubmittedData;
-  jumpDistance: JumpDistanceData;
-  launchedDrones: LaunchedDronesData;
-  materialCollected: MaterialCollectedData;
-  miningRefined: MiningRefinedData;
-  missionsAbandoned: MissionsAbandonedData;
-  missionsAccepted: MissionsAcceptedData;
-  missionsCompleted: MissionsCompletedData;
-  missionsFailed: MissionsFailedData;
-  prospectedAsteroids: ProspectedAsteroidsData;
-  scanned: ScannedData;
-  detailedScan: DetailedScanData;
-  cargoScan: CargoScanData;
-  soldExplorationData: SoldExplorationDataData;
+  [key: string]: number | string | undefined;
 }
 
 i18n.configure({
@@ -63,9 +15,9 @@ export function setLocale(lang: string): void {
   i18n.setLocale(lang);
 }
 
-export function t<K extends keyof TranslationData>(
-  key: K,
-  data?: TranslationData[K]
+export function t(
+  key: string,
+  data?: TranslationData
 ): string | string[] | undefined {
   const catalog = i18n.getCatalog(i18n.getLocale());
   const text: string[] = [];
